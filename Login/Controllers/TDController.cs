9fae7750985529cd27305f79076779b3217e69be
俊 池邊 <shun.ikebe@avantcorp.com>
2017-08-18 12:18:13 +0900

    プロジェクト ファイルを追加します。

---
+++ Login/Controllers/TDController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Login.Controllers
+{
+    public class TDController : Controller
+    {
+        // GET: TD
+        public ActionResult Index()
+        {
+            return File("~/Views/TD/index.html", "text/html");
+        }
+    }
+}
